fc9564ccd7a99f4fb794e27690f19c9ebf64ebc0
Basit Warsi <basitwarsi2595@gmail.com>
2025-12-28 17:46:31 +0530

    comm

--- frontend/app/api/process/route.ts
+++ frontend/app/api/process/route.ts
@@ -1,9 +1,12 @@
+export const runtime = "nodejs";
+export const dynamic = "force-dynamic";
+
 export async function POST(req: Request) {
-  const body = await req.formData();
+  const formData = await req.formData();
 
   const res = await fetch("http://13.60.91.241:8000/process", {
     method: "POST",
-    body,
+    body: formData,
   });
 
   return new Response(res.body, {
